99fd6fc34a9d9e84b8315aaf7d97d6b71b1c72b7
Akash jaiswal <akash.jaiswal@opstree.com>
2020-05-13 17:58:48 +0530

    [master][added][update jenkins file ]

---
+++ src/testAPI.py
@@ -0,0 +1,36 @@
+
+#change on lander to other Lander.
+
+import requests
+import json
+import time
+
+
+
+
+URL = "https://hdhdpxipga.execute-api.us-east-1.amazonaws.com/dev/my-provider/{host}/FeatureServer/0"
+
+success_list = []
+failure_list = []
+
+
+def ApiCheckHealth():
+    try:
+        res = requests.get(URL)
+       # time.sleep(0.5)
+        if res.status_code == 200:
+            print("API "+ URL + "STATUS ==>" + str(res.status_code))
+            #print("STATUS :",res.status_code)
+        else:
+            print("API "+ URL + "STATUS ==>" + str(res.status_code))  
+            
+    except Exception as er:
+        print("error",er)
+
+
+def main():
+    ApiCheckHealth()
+
+
+if __name__ == "__main__":
+    main()
