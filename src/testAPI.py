89556f9355fb196931876c7cc661acbd0ed80cb4
Akash jaiswal <akash.jaiswal@opstree.com>
2020-05-13 17:42:15 +0530

    [master][added][update jenkins file ]

--- src/testAPI.py
+++ src/testAPI.py
@@ -9,6 +9,7 @@ import time
 
 
 URL = "https://hdhdpxipga.execute-api.us-east-1.amazonaws.com/dev/my-provider/{host}/FeatureServer/0"
+
 success_list = []
 failure_list = []
 
@@ -16,11 +17,11 @@ failure_list = []
 def ApiCheckHealth():
     try:
         res = requests.get(URL)
-            time.sleep(0.5)
-            if res.status_code == 200:
-                print("Api is up now")
-            else:
-                print("Api is not Up")      
+       # time.sleep(0.5)
+        if res.status_code == 200:
+            print("STATUS :OK")
+        else:
+            print("STATUS : NOT UP")      
             
     except Exception as er:
         print("error",er)
